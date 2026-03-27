5c15c833de8434f9c9fc807c70a5af39dabe305b
Seraphin Eyala <eyalas472@gmail.com>
2026-03-27 15:45:49 +0100

    feat: affichage des données météo depuis OpenWeatherMap

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { WeatherService } from '../../services/weather';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class Home {}
+export class Home implements OnInit {
+  meteo = signal<any>(null)
+  loading = signal(true)
+  ville = signal('Paris')
+
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit() {
+    this.chargerMeteo()
+  }
+
+  chargerMeteo() {
+    this.loading.set(true)
+    this.weatherService.getMeteo(this.ville())
+      .subscribe({
+        next: (data) => {
+          this.meteo.set(data)
+          this.loading.set(false)
+        },
+        error: (err) => {
+          console.error(err)
+          this.loading.set(false)
+        }
+      })
+  }
+}
---
+++ src/app/services/weather.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  private baseUrl = 'https://api.openweathermap.org/data/2.5'
+
+  constructor(private http: HttpClient) {}
+
+  getMeteo(ville: string) {
+    return this.http.get(`${this.baseUrl}/weather`, {
+      params: {
+        q: ville,
+        appid: environment.apiKey,
+        units: 'metric',
+        lang: 'fr'
+      }
+    })
+  }
+}
